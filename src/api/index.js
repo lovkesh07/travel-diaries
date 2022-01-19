7d3392775053ecc41cd3f3094202d95e2692c63e
lovkesh07 <79805511+lovkesh07@users.noreply.github.com>
2022-01-19 18:17:02 +0530

    second commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,37 +1,47 @@
 import axios from 'axios';
 
-const URL ='https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
+const URL = 'https://travel-advisor.p.rapidapi.com/locations/v2/auto-complete';
+
 
 const options = {
   //method: 'GET',
-  //url: 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary',
-  params: {
-    bl_latitude: '11.847676',
-    tr_latitude: '12.838442',
-    bl_longitude: '109.095887',
-    tr_longitude: '109.149359',
-    
-  },
+//  url: 'https://travel-advisor.p.rapidapi.com/locations/v2/auto-complete',
+  params: {query: 'eiffel tower', lang: 'en_US', units: 'km'},
   headers: {
     'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
     'x-rapidapi-key': '850d5affb0msh5f331611f71a851p1a9329jsn0a69a4070853'
   }
 };
 
-export const getWeatherData = async () => {
-    try {
-      
-        const {data: {data }} = await axios.get( URL,options);
-          //params: { lat, lon: lng },
-        //  headers: {
-         //   'x-rapidapi-key': process.env.REACT_APP_RAPID_API_WEATHER_API_KEY,
-        //    'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
-       //   },
-       // });
-  
-        return data;
-      
-    } catch (error) {
-      console.log(error);
-    }
+
+export const getPlacesData = async () => {
+  try{
+    const {data:{data}} = await axios.get(URL, options);
+
+    return data;
+  }catch(error){
+      console.log(error)
   }
+}
+
+
+export const getWeatherData = async (lat,lng) => {
+  try{
+    const {data} = await axios.get('https://community-open-weather-map.p.rapidapi.com/weather' , {
+      params: {
+
+        lat: lat,
+        lon: lng,
+      },
+      headers: {
+        'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+        'x-rapidapi-key': '850d5affb0msh5f331611f71a851p1a9329jsn0a69a4070853'
+      }
+        
+    });
+
+    return data;
+  }catch(error){
+      console.log(error)
+  }
+}
